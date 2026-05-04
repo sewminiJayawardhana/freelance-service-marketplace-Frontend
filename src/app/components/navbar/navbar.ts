e6c268e6b257a1d64de1c0f5007cb848e3cbfded
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2026-05-04 22:34:50 +0530

    feat: integrate navbar with app component and fix standalone component import errors

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  constructor(private router: Router) {}
+
+  // යූසර් ලොග් වෙලාද කියලා බලනවා (Token එකක් තියෙනවාද කියලා බලනවා)
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  // Logout වෙන හැටි
+  onLogout() {
+    localStorage.removeItem('token'); // Token එක අයින් කරනවා
+    this.router.navigate(['/login']); // Login එකට යවනවා
+  }
+}
